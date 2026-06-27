6d045fc0e0e18efc6e333b8c971fb3c28102dbb8
Dalvae <diego.alvarez.e@ug.uchile.cl>
2026-06-27 12:33:08 -0400

    chore: actualizar configuracion OpenAPI

--- src/main/java/cl/duoc/transportista/despacho/config/OpenApiConfig.java
+++ src/main/java/cl/duoc/transportista/despacho/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package cl.duoc.transportista.despacho.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,16 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "bearer-jwt";
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Pegar access token Azure/Entra ID sin el prefijo Bearer")))
                 .info(new Info()
                         .title("API Gestion de Guias de Despacho")
                         .version("1.0")
